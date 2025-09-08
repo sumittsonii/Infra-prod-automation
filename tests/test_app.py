e721c0fa2c326b128e83de1e90d65ea16c752ba7
sumittsonii <73901584+sumittsonii@users.noreply.github.com>
2025-09-08 16:41:50 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,8 @@
-def test_healthcheck():
-    assert 1 + 1 == 2
+import pytest
+from app import app
+
+def test_hello_route():
+    tester = app.test_client()
+    response = tester.get("/")
+    assert response.status_code == 200
+    assert b"welcome to website deployed using a CI/CD pipeline." in response.data
