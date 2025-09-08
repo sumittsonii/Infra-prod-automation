fd5ebcfc65467bb5f486a59960354f725b7e2672
sumittsonii <73901584+sumittsonii@users.noreply.github.com>
2025-09-08 16:44:17 +0530

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,8 @@
+import pytest
+from app import app
+
+def test_hello_route():
+    tester = app.test_client()
+    response = tester.get("/")
+    assert response.status_code == 200
+    assert b"welcome to website deployed using a CI/CD pipeline." in response.data
