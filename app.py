87e80cd8d20a8de8d5e149975c3482499ee187b3
sumittsonii <73901584+sumittsonii@users.noreply.github.com>
2025-09-08 17:37:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return """
-    <!doctype html>
-    <html><body style="font-family:system-ui, -apple-system, Segoe UI, Roboto, sans-serif; line-height:1.5; padding:2rem;">
-      <p>Hey there,</p>
-      <p>welcome to website deployed using a CI/CD pipeline.</p>
-      <p>Thanks,<br>Sumit</p>
+    return (
+        "<!doctype html>"
+        "<html><body style=\"font-family:system-ui, -apple-system, Segoe UI, Roboto, "
+        "sans-serif; line-height:1.5; padding:2rem;\">"
+        "<p>Hey there,</p>"
+        "<p>welcome to website deployed using a CI/CD pipeline.</p>"
+        "<p>Thanks,<br>Sumit</p>"
+        "</body></html>"
+    )
 
-    </body></html>
-    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
