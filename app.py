ba217a10d0c36799f7e1d6f518871d58c0ae6766
sumittsonii <73901584+sumittsonii@users.noreply.github.com>
2025-09-08 16:38:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     return """
     <!doctype html>
-    <html><body style="font-family:system-ui, -apple-system, Segoe UI, Roboto, 
+    <html><body style="font-family:system-ui, -apple-system, Segoe UI, Roboto,
     sans-serif; line-height:1.5; padding:2rem;">
       <p>Hey there,</p>
       <p>welcome to website deployed using a CI/CD pipeline.</p>
